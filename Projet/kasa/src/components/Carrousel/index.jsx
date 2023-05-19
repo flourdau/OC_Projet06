6e605db46eb258362be7ecbfae2042687c1cba11
flourdau <lourdaux.florian.pro@gmail.com>
2023-05-19 10:11:59 +0200

    Carrousel clean

--- Projet/kasa/src/components/Carrousel/index.jsx
+++ Projet/kasa/src/components/Carrousel/index.jsx
@@ -6,10 +6,6 @@ import Next from '../../assets/next.svg'
 
 import './Carrousel.css'
 
-// function handleClick (switchPicture) {
-//     console.log(switchPicture)
-// }
-
 export default function Carousel({ pictures }) {
 
     const [ id, setId ] = useState(1)
@@ -24,17 +20,12 @@ export default function Carousel({ pictures }) {
     return (
 
         <section className='carrousel'>
-
             <div>
                 <img src={ Previous } alt={ Previous } onClick={() => handleClick('previous', pictures)} />
                 <img src={ Next } alt={ Next } onClick={() => handleClick('next', pictures)}/>
             </div>
             <p className="carousel_cnt"> {id}/{lenPictures}</p>
             <img className="carousel_pic" src={pictures[id - 1]} alt="Représentation du logement" />
-            {/* <ul>{ pictures.map((element) => (
-                <li><img key={ `${element}` } src={ `${element}` } alt={` ${element}` } /></li>)) }
-            </ul> */}
-
         </section>
 
     )
