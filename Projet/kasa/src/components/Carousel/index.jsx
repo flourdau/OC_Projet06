ad03d2b33d14a4655a26c0a0649147b82a4cd127
flourdau <lourdaux.florian.pro@gmail.com>
2023-05-24 10:29:18 +0200

    Correction carousel with one picture

---
+++ Projet/kasa/src/components/Carousel/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import propTypes from 'prop-types'
+
+import Previous from '../../assets/previous.svg'
+import Next from '../../assets/next.svg'
+
+import './Carousel.css'
+
+export default function Carousel({ pictures }) {
+
+    const [ id, setId ] = useState(1)
+    const lenPictures = pictures.length
+    let myClassName ='carousel_btn'
+
+    if (lenPictures <= 1) {
+        myClassName = 'hiddenBtn'
+    }
+
+    const handleClick = switchPicture => (
+        switchPicture === 'next'
+            ? (setId(id === lenPictures ? 1 : id + 1))
+            : (setId(id === 1 ? lenPictures : id - 1))
+    )
+
+    return (
+        <section className='carousel'>
+            <img className="carousel_pic" src={ pictures[id - 1] } alt="Représentation du logement" />
+            <div className={ myClassName }>
+                <img src={ Previous } alt={ Previous } onClick={() => handleClick('previous', pictures)} />
+                <p className="carousel_cnt"> { id }/{ lenPictures }</p>
+                <img src={ Next } alt={ Next } onClick={() => handleClick('next', pictures)}/>
+            </div>
+        </section>
+
+    )
+
+}
+
+Carousel.propTypes = {
+    pictures: propTypes.arrayOf(propTypes.string)
+}
