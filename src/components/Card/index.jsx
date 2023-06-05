e32d1aba0560807a33f612573f1a112350654386
flourdau <lourdaux.florian.pro@gmail.com>
2023-06-05 15:23:19 +0200

    Cover correction

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 import './Card.css'
 
 export default function Card({ id, title, cover }) {	
+
+	const background = {
+		background: `url("${cover}")`,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center'
+	}
+
 	return (
-		<Link className='kasa-item' to={ `/product/${ id }` } >
+		<Link className='kasa-item' to={ `/product/${ id }` } style={ background }>
 			<h2>{ title }</h2>
 		</Link>
 )}
